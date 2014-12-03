9a173567ca51d91db43dc15a487deee1449e2af5
Doug Graiser <dmgraiser@gmail.com>
2014-12-03 15:44:08 -0500

    rolled back to remove users scaffold and fixed so events still working

--- app/views/users/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @user, :id, :name, :email, :message, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -32,8 +32,6 @@ Rails.application.routes.draw do
 
   resources :events
 
-  resources :users
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
