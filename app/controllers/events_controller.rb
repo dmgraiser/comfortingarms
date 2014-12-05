e231a1ea487027d7cdd345f5ee04e2e58df695a5
Doug Graiser <dmgraiser@gmail.com>
2014-12-05 11:38:14 -0500

    deleted old users table and added devise

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,8 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
+  before_filter :authenticate_user!
+
   # GET /events
   # GET /events.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   get 'about' => 'welcome#about'
 
   get 'services' => 'welcome#services'
